9b1eab3d69d242da2817b0888a0173ac1e7b37f6
EL <prometheus.lie@gmail.com>
2022-06-21 12:21:29 +0800

    add check email exist.

--- src/__tests__/Register.test.ts
+++ src/__tests__/Register.test.ts
@@ -1,9 +1,11 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, getIdToken } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, getIdToken, UserCredential } from "firebase/auth";
 import { getMessaging, getToken, Messaging } from "firebase/messaging";
+import { getStorage, ref, uploadBytes, uploadString } from "firebase/storage";
 import { print } from 'graphql';
 import gql from 'graphql-tag';
 import axios from 'axios';
+import * as fs from "fs";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -21,18 +23,45 @@ const auth = getAuth(app);
 // This browser doesn't support the API's required to use the Firebase SDK.
 // const messaging = getMessaging(app);
 const endpoint = "https://cloud-run-api-psbeauty-deuedjpwuq-de.a.run.app/api/graphql/";
-//const endpoint = "http://localhost:8080/api/graphql/";
+// const endpoint = "http://localhost:8080/api/graphql/";
 const email = "el.lai@cloudlytics.me"
 const password = "12341234"
 
 test('createUserWithEmailAndPassword', async () => {
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    // 設定過程不得超過一分鐘
+    jest.setTimeout(60000);
+
+    // 先檢查 email 是否已經註冊
+    const EMAIL_EXISTS = gql`
+    query emailExists($email: String){
+        emailExists(email:  $email) {
+            exists
+        }
+    }`;
+    const existResult = await axios.post(endpoint, {
+        query: print(EMAIL_EXISTS),
+        variables: {
+            email: email
+        },
+    });
+    expect(existResult.data.data.emailExists.exists).toBeFalsy();
+
+    let userCredential: UserCredential;
+    try {
+        // 嘗試註冊
+        userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    } catch (signUpError: any) {
+        expect(signUpError).toBeUndefined();
+        return;
+    }
+    // 使用者憑證必須有
     expect(userCredential).not.toBeUndefined();
 
     const idToken = await userCredential.user.getIdToken(true);
     console.log(`idToken : ${idToken}`);
-    // Get device token for firebase cloud messaging.
-    //const client_device_token = getToken(messaging);
+    // 取得 firebase cloud messaging 所需之裝置識別碼，但此程式無法在 nodejs 中執行，
+    // 必須在瀏覽器中方能取得。
+    // const client_device_token = getToken(messaging);
     const client_device_token = "firebase_client_device_token";
     expect(client_device_token).not.toBeUndefined();
 
@@ -52,7 +81,7 @@ test('createUserWithEmailAndPassword', async () => {
         query: print(ADD_USER),
         variables: {
             phone: "0909090909",
-            name: "Wang",
+            name: "El Lai",
             clientToken: [client_device_token],
         },
     }, {
@@ -61,11 +90,13 @@ test('createUserWithEmailAndPassword', async () => {
             'Authorization': `Bearer ${idToken}`
         }
     })
-    console.log(result.data);
+    // console.log(result.data);
     expect(result.data.data.addUser.id).not.toBeUndefined();
 });
 
 test('signInWithEmailAndPassword', async () => {
+    jest.setTimeout(60000);
+
     const userCred = await signInWithEmailAndPassword(auth, email, password);
     const idToken = await getIdToken(userCred.user, false);
     console.log(`idToken: ${idToken}`);
@@ -79,13 +110,13 @@ test('signInWithEmailAndPassword', async () => {
     }
     `
     const result = await axios.post(endpoint, {
-        query: print(CUSTOM_TOKEN),        
+        query: print(CUSTOM_TOKEN),
     }, {
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${idToken}`
         }
-    })
+    })    
     console.log(result.data);
     expect(result.data.data.customToken.customToken).not.toBeUndefined();
 });
